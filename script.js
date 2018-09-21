75dda5463af8f3f255bfddd826675d482e07db78
Colin Boehning <colin.boehning@gmail.com>
2018-09-21 14:15:40 +0200

    inital Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const bookroom = new Vue({
+    el: '#button',
+    data: {
+        buttonText: "Book a Room",
+    }
+
+});
+
+const bookings = new Vue({
+    el: '#bookings',
+    data: {
+        bookingsbutton1: "Room: Rick 15.09.2018 12:00 - 15:00",
+        bookingsbutton2: "Room: Morty 05.09.2018 12:00 - 15:00",
+    }
+
+});
+
+
