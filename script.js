58ecac0c4a8a5da7750f6f4f8615f282218036da
Colin Boehning <colin.boehning@gmail.com>
2018-09-24 19:34:18 +0200

    first Screen -> second Screen Interaction with Vue Templates and V-on:click and v-show

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const bookroom = new Vue({
-    el: '#button',
-    data: {
-        buttonText: "Book a Room",
-    }
 
-});
-
-const bookings = new Vue({
-    el: '#bookings',
+const app = new Vue({
+    el: '#app',
     data: {
         bookingsbutton1: "Room: Rick 15.09.2018 12:00 - 15:00",
         bookingsbutton2: "Room: Morty 05.09.2018 12:00 - 15:00",
+        buttonText: "Book a Room",
+        calendar: "here should be a calendar",
+        nextScreen: false
+
     }
 
 });
+
 
 
